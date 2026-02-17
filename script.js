7afe5e20689629bb073b9befb46cdb6615cbc760
Silovik77 <laver138@gmail.com>
2026-02-17 11:01:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Укажите ваш реальный URL Amvera (без пробелов!)
 const API_URL = 'https://silovik-silovik.waw0.amvera.tech';
-const BOT_USERNAME = "@gynecologistkrd_bot";
+const BOT_USERNAME = "gynecologistkrd_bot";
 // --- Словари перевода ---
 const MAP_TRANSLATIONS = {
  "Dam ":  "Плотина ",
@@ -379,3 +379,4 @@ function openBotChatAndSendCommand(command) {
 document.addEventListener('DOMContentLoaded', () => {
   showMainMenu();
 });
+
